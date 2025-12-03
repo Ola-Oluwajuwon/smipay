09cc9937a2205b4f83c681046f67d59b923f0332
Ola-Oluwajuwon <okayode986@stu.ui.edu.ng>
2025-12-03 18:23:21 +0100

    Fix: add a little padding for stack card container affect by positioning.

--- components/home/StackCard.tsx
+++ components/home/StackCard.tsx
@@ -68,7 +68,7 @@ const cards = [
 
 export default function StackCard() {
   return (
-    <section className="mx-auto max-w-7xl px-4 md:px-6 py-16 md:py-24">
+    <section className="mx-auto max-w-7xl px-4 md:px-6 py-16 md:py-24 pb-30">
       {/* Heading and Description */}
       <div className="text-center max-w-3xl mx-auto mb-12 md:mb-16">
         <h2 className="text-brand-text-primary text-3xl md:text-4xl lg:text-4xl font-semibold mb-4">
